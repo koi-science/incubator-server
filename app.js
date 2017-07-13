506e049de2f0760b1baf792ada3cceb112787210
Tatiana Baklan <baklantat@gmail.com>
2017-07-13 18:36:01 +0800

    echo post request

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var logger = require('morgan');
 var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
-var users = require('./routes/users');
+var temperature = require('./routes/temperature');
 
 var app = express();
 
@@ -17,7 +17,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/', index);
-app.use('/users', users);
+app.use('/temperature', temperature);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
