cad95468e2a4519065f3f5ee99e6ae1493535b4a
Tatiana Baklan <baklantat@gmail.com>
2017-07-27 00:48:23 +0800

    display temperature for one day

---
+++ constant/index.js
@@ -0,0 +1,11 @@
+const CONSTANTS = {
+    HOUR: 60 * 60 * 1000,
+    DAY:  60 * 60 * 1000 * 24,
+    WEEK: 60 * 60 * 1000 * 24 * 7,
+    MONTH: 60 * 60 * 1000 * 24 * 30 // TODO add logic to month
+};
+
+module.exports = CONSTANTS;
+
+
+
--- routes/devices.js
+++ routes/devices.js
@@ -1,6 +1,7 @@
 const express = require('express'),
       router = express.Router(),
-      Incubator = require('../models/incubator');
+      Incubator = require('../models/incubator'),
+      CONSTANTS = require('../constant/index');
 
 
 router.get('/devices', (req, res) => {
@@ -15,5 +16,36 @@ router.get('/device/:deviceId', (req, res) => {
     });
 
 });
+
+router.get('/device/:deviceId/hourData', (req, res) => {
+    Incubator.find({
+        deviceId: req.params.deviceId,
+        timeStamp: {
+            $gt: new Date().valueOf() - CONSTANTS.HOUR
+        }
+    }, function(error, data) {
+        res.send(data);
+    });
+});
+
+router.get('/device/:deviceId/dayData', (req, res) => {
+    Incubator.find({deviceId: req.params.deviceId}, function(error, data) {
+        res.send(data);
+    });
+});
+
+router.get('/device/:deviceId/weekData', (req, res) => {
+    Incubator.find({deviceId: req.params.deviceId}, function(error, data) {
+        res.send(data);
+    });
+});
+
+router.get('/device/:deviceId/monthData', (req, res) => {
+    Incubator.find({deviceId: req.params.deviceId}, function(error, data) {
+        res.send(data);
+    });
+});
+
+
 
 module.exports = router;
