14bffd975cf0cc26318e71c583c73949f9614a4c
Tatiana Baklan <baklantat@gmail.com>
2017-07-27 12:33:20 +0800

    implement api for fetching temperaure data

--- routes/devices.js
+++ routes/devices.js
@@ -21,7 +21,7 @@ router.get('/device/:deviceId/hourData', (req, res) => {
     Incubator.find({
         deviceId: req.params.deviceId,
         timeStamp: {
-            $gt: new Date().valueOf() - CONSTANTS.HOUR
+            $gte: new Date().valueOf() - CONSTANTS.HOUR
         }
     }, function(error, data) {
         res.send(data);
@@ -29,19 +29,49 @@ router.get('/device/:deviceId/hourData', (req, res) => {
 });
 
 router.get('/device/:deviceId/dayData', (req, res) => {
-    Incubator.find({deviceId: req.params.deviceId}, function(error, data) {
+    Incubator.find({
+        deviceId: req.params.deviceId,
+        timeStamp: {
+            $gte: new Date().valueOf() - CONSTANTS.DAY
+        }
+    }, function(error, data) {
         res.send(data);
     });
 });
 
 router.get('/device/:deviceId/weekData', (req, res) => {
-    Incubator.find({deviceId: req.params.deviceId}, function(error, data) {
+    Incubator.find({
+        deviceId: req.params.deviceId,
+        timeStamp: {
+            $gte: new Date().valueOf() - CONSTANTS.WEEK
+        }
+    }, function(error, data) {
         res.send(data);
     });
 });
 
 router.get('/device/:deviceId/monthData', (req, res) => {
-    Incubator.find({deviceId: req.params.deviceId}, function(error, data) {
+    Incubator.find({
+        deviceId: req.params.deviceId,
+        timeStamp: {
+            $gte: new Date().valueOf() - CONSTANTS.MONTH
+        }
+    }, function(error, data) {
+        res.send(data);
+    });
+});
+
+router.get('/device/:deviceId/arbitraryData', (req, res) => {
+    let start = req.param.start,
+        end = req.param.end;
+
+    Incubator.find({
+        deviceId: req.params.deviceId,
+        timeStamp: {
+            $gte: start,
+            $lte: end
+        }
+    }, function(error, data) {
         res.send(data);
     });
 });
